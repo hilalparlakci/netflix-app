1aaac2437621e9a96f0f1929fe0f85f561c905c7
Hilal <hilalparlakci>
2023-09-14 18:32:39 +0300

    deneme

---
+++ NetflixApp/NetflixAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixAppApp.swift
+//  NetflixApp
+//
+//  Created by Hilal on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetflixAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
